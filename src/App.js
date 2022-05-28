f1b05461e07ef51cc6255addf58d01206953e6ec
dfg1425@naver.com <dfg1425@naver.com>
2022-05-28 20:36:35 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  // Route 컴포넌트에 path와 컴포넌트를 설정하여 나열해준다.
+  // BrowserRouter로 Route 들을 감싸준다.
+  // 브라우저에서 요청한 경로에 Route의 Path가 들어있으면 해당 component를 보여준다.
+
+  // exact를 추가적으로 사용해서 완전히 똑같은 path와 url이어야 매칭되게 해줌
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* Switch 사용해 url 맞는 게 없으면 not found로, 가장 넓은 경로를 맨 밑으로 -> 루트경로는 맨밑 */}
+      <Switch>
+        {/* id 받아서 사용가능 */}
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" component={About} />
+        <Route path="/" exact component={Home} />
+        {/* 다 찾지 못했을 때 여기로 */}
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
