90b2fcdf1778761a73337e6b6375d3710980a892
dfg1425@naver.com <dfg1425@naver.com>
2022-05-29 20:19:16 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import Links from "./components/Links";
+import NavLinks from "./components/NavLinks";
+import Login from "./pages/Login";
+import { Redirect } from "react-router-dom";
+
+const isLogin = false;
 
 function App() {
   // Route 컴포넌트에 path와 컴포넌트를 설정하여 나열해준다.
@@ -12,8 +18,17 @@ function App() {
   // exact를 추가적으로 사용해서 완전히 똑같은 path와 url이어야 매칭되게 해줌
   return (
     <BrowserRouter>
+      {/* a 태그가 아니라 Link to로 화면 이동하기... 로딩표시 X 새로운 파일다운 X, 페이지 이동 더 효율적으로 가능 */}
+      {/* Links.jsx를 가져와서 랜더링만 해주면 됨 */}
+      <Links />
       {/* Switch 사용해 url 맞는 게 없으면 not found로, 가장 넓은 경로를 맨 밑으로 -> 루트경로는 맨밑 */}
+      <NavLinks />
       <Switch>
+        <Route
+          path="/login"
+          render={() => (isLogin ? <Redirect to="/" /> : <Login />)}
+        />
+        <Route path="/login" component={Login} />
         {/* id 받아서 사용가능 */}
         <Route path="/profile/:id" component={Profile} />
         <Route path="/profile" component={Profile} />
