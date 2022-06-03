b47cec1de321afa11f0e6da899849f5eb5bcf4b3
dfg1425@naver.com <dfg1425@naver.com>
2022-06-04 00:19:43 +0900

    useHistory

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,12 +1,23 @@
 // withRouter를 사용하면 route에서 연결된 props를 다 넣어준다.
 import { withRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-export default withRouter(function LoginButton(props) {
-  console.log(props);
+// export default withRouter(function LoginButton(props) {
+//   console.log(props);
+//   function login() {
+//     setTimeout(() => {
+//       props.history.push("/");
+//     }, 1000);
+//   }
+//   return <button onClick={login}>로그인하기</button>;
+// });
+
+export default function LoginButton(props) {
+  const history = useHistory();
   function login() {
     setTimeout(() => {
-      props.history.push("/");
+      history.push("/");
     }, 1000);
   }
   return <button onClick={login}>로그인하기</button>;
-});
+}
